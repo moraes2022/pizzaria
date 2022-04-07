fd9bc53194c8afab38f342971add99681081d90b
Mr. Moraes <mr.moraess@outlook.com>
2022-04-07 13:46:15 -0300

    Verification phase 7: Display a page with img pizzas

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,7 +1,10 @@
+
+
+//Importe o conteúdo do arquivo Pizzas.json para uma variável chamada pizzas. @done(22-04-06 22:04)
+let pizzas = require('../database/Pizzas.json');
+
 //No arquvo PizzasController.js crie um objeto literal vazio e o exporte utilizando o module.exports @done(22-04-06 15:25)
 module.exports = {
-    listar:(req, res) => {
-        res.render('pizzas.ejs')
-    }
-    
-    };
+    listar: (req,res) => {
+        res.render('pizzas.ejs', {pizzas, busca:''});              
+    }}
